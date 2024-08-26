87767780812be61d183e0261df0320f89f42da74
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-08-26 20:00:51 +0000

    Start of class on 26 Aug 2024

---
+++ ypatel10/topic-01-simple-expressions/nodes.md
@@ -0,0 +1,14 @@
+
+# Notes
+- We will use regular expressions, pattern matching
+- eg can be:  ls *.py
+
+- [abcd] - will match anything inside the []
+- matching a single digit([0123456789]+)
+- (1)
+- ((1))
+- (((1)))
+- (*[0123456789]+)*
+- cannot write a regex that can match the number of start and end parens
+
+# Notes for Tue, 26 Aug 2024
--- ypatel10/topic-01-simple-expressions/tokenizer.py
+++ ypatel10/topic-01-simple-expressions/tokenizer.py
@@ -3,15 +3,6 @@
 # author: Yug Patel
 # last modified: 21 Aug 2024
 
-# We will use regular expressions, pattern matching
-# eg can be:  ls *.py
 
-# [abcd] - will match anything inside the []
-# matching a single digit([0123456789]+)
-# (1)
-# ((1))
-# (((1)))
-# (*[0123456789]+)*
-# cannot write a regex that can match the number of start and end parens
 if __name__ == "__main__":
     pass
