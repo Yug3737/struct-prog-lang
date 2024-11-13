af8263a7dc72b955e24b0ba97cb99b389a273370
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-11-13 21:09:27 +0000

    Before class on 13 Nov

---
+++ ypatel10/topic-05-functions/temp.py
@@ -0,0 +1,12 @@
+ast = {
+    "tag": "if",
+    "condition": {"tag": "number", "value": 1, "position": 3},
+    "then": {
+        "tag": "print",
+        "value": {"tag": "number", "value": 2, "position": 11},
+    },
+    "else": {
+        "tag": "print",
+        "value": {"tag": "number", "value": 3, "position": 23},
+    },
+}
